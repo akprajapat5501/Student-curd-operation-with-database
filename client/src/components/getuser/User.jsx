c54ae96bf14559af6e4cae908b1992efbe9cba05
AAKASH KUMAR <162182656+akprajapat5501@users.noreply.github.com>
2025-08-30 13:38:21 +0530

    Update User.jsx

--- client/src/components/getuser/User.jsx
+++ client/src/components/getuser/User.jsx
@@ -9,14 +9,14 @@ const User = () => {
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await axios.get("http://127.0.0.1:8000/api/getAll")
+            const response = await axios.get("https://student-curd-operation-with-database.onrender.com/api/getAll")
             setUsers(response.data);
         };
         fetchData()
     }, [])
 
     const deleteUser = async(userId) =>{
-        await axios.delete(`http://127.0.0.1:8000/api/delete/${userId}`)
+        await axios.delete(`https://student-curd-operation-with-database.onrender.com/api/delete/${userId}`)
         .then((response)=>{
             setUsers((prevUser)=> prevUser.filter((user)=> user._id !== userId));
             toast.success(response.data.msg, {position: "top-right"})
